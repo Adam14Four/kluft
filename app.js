fc0001a558e2a4c4a15316056f030d1c6e319164
caleb jeffrey <calebjeffrey.dev@gmail.com>
2015-02-18 08:31:46 -0800

    email subject copy change

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/form', function(req, res) {
             sendgrid.send({
                 to: req.body.email,
                 from: 'QandA@ESKluft.com',
-                subject: 'Re: Kluftmattress contact',
+                subject: 'Re: your Kluft request',
                 text: 'Hello,\nThank you for reaching out to E.S. Kluft & Company. Your query is important to us.\nWe typically answer emails within 2 business days of receiving them. Our business hours are Monday through Friday 10AM – 6PM PST.\n\nSincerely,\nThe E.S. Kluft & Company team'
             });
         });
