fea3998a1107127dc7eaa4a8b2e1594dcc0a94fa
caleb jeffrey <calebjeffrey.dev@gmail.com>
2015-10-12 02:02:51 -0700

    new tradition page work

--- static/js/app/views/TraditionView.js
+++ static/js/app/views/TraditionView.js
@@ -16,7 +16,9 @@ define(function(require, exports, module) {
 
         ui: {
             parallaxBg: '.parallax-bg',
-            textbox: '.parallax-bg .text-box'
+            textbox: '.parallax-bg .text-box',
+            box1: '.block-text-1',
+            caption: '.masthead-caption.desktop'
         },
 
         initialize: function(options) {
@@ -29,6 +31,32 @@ define(function(require, exports, module) {
 
         onDestroy: function() {
             this.window.off('.about');
+        },
+
+        onResize: function() {
+            if (!Modernizr.touch) {
+                var headerHeight = $('#region-header').height();
+                this.$el.css('margin-top', '-' + headerHeight + 'px');
+                this.ui.textbox.css('margin-top', headerHeight/2);
+            }
+
+            this.winWidth = $(window).width();
+
+            this.ui.caption.css('left', this.ui.box1.offset().left);
+
+            if (this.winWidth >= 960 && this.winWidth < 1100) {
+                this.bgYOffset = -60;
+            } else if (this.winWidth >= 1100 && this.winWidth < 1300) {
+                this.bgYOffset = -100;
+            } else if (this.winWidth >= 1300 && this.winWidth < 1500) {
+                this.bgYOffset = -150;
+            } else if (this.winWidth >= 1500) {
+                this.bgYOffset = -200;
+            } else {
+                this.bgYOffset = 0;
+            }
+
+            this.scrollEffects();
         }
 
     });
