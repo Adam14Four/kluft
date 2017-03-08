3e37328a8d45caf94bcef9b872a9bee221ca1957
calebjeffrey <calebjeffrey.dev@gmail.com>
2017-03-08 12:02:49 -0800

    add gmaps api key

--- static/js/app/utils/gmaps.js
+++ static/js/app/utils/gmaps.js
@@ -3,7 +3,7 @@ define(function(require, exports, module) {
         init: function(options) {
             options = options || {};
             if (typeof window.google === 'undefined' || typeof window.google.maps === 'undefined') {
-                require(['async!http://maps.google.com/maps/api/js?sensor=false'], function() {
+                require(['async!http://maps.google.com/maps/api/js?key=AIzaSyDrN-4xxe3I8wUWJJXndcmuZGYyj2EzaJg'], function() {
                     if (typeof options.success === 'function') {
                         options.success(window.google);
                     }
